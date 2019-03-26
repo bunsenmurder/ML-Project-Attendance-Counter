96d8295b483a554cb0cf6a9d9a3ff8a8691455c5
bunsenmurder <ErichMengore@gmail.com>
2019-03-26 16:15:35 -0400

    [Use Gitbash/Unix] Run the script in the tar folder first
    The script gets the ImageDB folder setup, which hosts the split tar archive of our dataset.

---
+++ tar/runFirst.py
@@ -0,0 +1,11 @@
+import tarfile
+import shutil
+import subprocess
+import os
+
+com_cmd = 'cat ImageDB.tar.bz2.aa ImageDB.tar.bz2.ab ImageDB.tar.bz2.ac > ImageDB.tar.bz2'
+p = subprocess.Popen(com_cmd, shell=True)
+os.waitpid(p.pid, 0)
+tf = tarfile.open('ImageDB.tar.bz2', 'r:bz2')
+tf.extractall(path = "..")
+print("ImageDB extraction complete")
