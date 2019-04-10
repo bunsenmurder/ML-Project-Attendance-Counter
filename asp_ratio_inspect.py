96f4d423c4a13f9b667d5e03c26acf6e7d23806c
bunsenmurder <ErichMengore@gmail.com>
2019-04-10 04:49:00 -0400

    Version 0.0.2
    Added more directions, added multiple models to test out in bash commands. Changed code to accommodate Caltech and new MIT faces datasets. Changed negative datasets to backgrounds.

--- asp_ratio_inspect.py
+++ asp_ratio_inspect.py
@@ -3,7 +3,6 @@ import os
 from PIL import Image
 from statistics import mean
 
-i = 1
 width = []
 height = []
 size = []
@@ -15,11 +14,36 @@ def round_down(num, divisor):
 def round_up(num, divisor):
     num = int(num)
     return num + (num%divisor)
+cwd = list(os.walk('.'))
+cwd = cwd[0][1]
+#path = 'training-synthetic'
+count = 0
+i = 0
+print('Inspector starting...','\n')
+for folders in cwd:
+	print(str(i),') ',cwd[i])
+	i+=1 
+print('Please select from an option above:')
+input = input()
+input = int(input)
+print('\n')
+path = cwd[input]
+for filenames in os.listdir(path):
+	if filenames.endswith('.jpg'):
+            count += 1
+            try:
+                im = Image.open(os.path.join(path,filenames))
+                im.verify()
+                im.close()
+            except(IOError, SyntaxError) as e:
+                print('Bad file: ', filenames)
+            width.append(im.width)
+            height.append(im.height)
+            size.append(im.size)
+            aspectratio = width[count-1]/height[count-1]
+            ar.append(aspectratio)
 
-path = 'ImageDB/Pos_cropped_no_scale'
-
-#Implement this error code with the random_sample.py file.
-
+'''		
 while i < 436:
     image = 'pos_' + str(i) + '.jpg'
     im = Image.open(path +'/' + image)
@@ -32,10 +56,11 @@ while i < 436:
     if width[i-1] > height[i-1]:
         print('The image ' + image + ' has a size of ', size[i-1], ' has a width larger than its height.\n')
     i= i +1
-
+'''
 avg_ar = mean(ar)
 avg_w = mean(width)
 avg_h = mean(height)
+
 
 # Images Information Print
 print('Minimum AR is: ' + str(min(ar)) + '\n')
@@ -44,9 +69,10 @@ print('Average AR is: ' + str(avg_ar) + '\n')
 print('Average Width is: ' + str(avg_w) + '\n')
 print('Average Height is: ' + str(avg_h) + '\n')
 print('Minimum Width is: ' + str(min(width)) + '\n')
-#print('Maximum Width is: ' + str(max(width)) + '\n')
+print('Maximum Width is: ' + str(max(width)) + '\n')
 print('Minimum Height is: ' + str(min(height)) + '\n')
-#print('Maximum Height is: ' + str(max(height)) + '\n')
+print('Maximum Height is: ' + str(max(height)) + '\n')
+print('Image count is: ' + str(count) + '\n')
 
 # These functions check the aspect ratios of the resolutions
 #which are rounded to the nearest multiples of 4 to determine
@@ -58,4 +84,4 @@ if r1 < r2:
 elif r2 < r1:
     print("The best resolution is Width: ", round_up(avg_w, 4)/2, " Height: ", round_up(avg_h, 4)/2)
 else:
-    print("The best resolution is the round deafault with  Width: ", int(avg_w)/4, " Height: ", int(avg_h)/2)
+    print("The best resolution is the round deafault with  Width: ", int(avg_w)/2, " Height: ", int(avg_h)/2)
--- random_sample.py
+++ random_sample.py
@@ -8,6 +8,7 @@ nfile=r'Images/RandomFile'
 filearray= ['Pos', 'Neg']
 
 i,j=0,0
+pos_set = 0
 
 print('How many positive samples do you want?')
 p = input()
@@ -16,7 +17,18 @@ pos_in = int(p)
 print('How many negative samples do you want?')
 n = input()
 neg_in = int(n)
-
+print('\n')
+print('1) Caltech 101 Faces','\n')
+print('2) MIT-CBCL Faces','\n')
+print('Please select a number representing an option above:')
+input = input()
+input = int(input)
+if input == 1:
+    pos_set = 435
+    file_ext = '.jpg'
+if input == 2:
+    pos_set = 3240
+    file_ext = '.pgm'
 while i < len(filearray):
     fold_p = path + '/' + filearray[i]
     fold_d= os.listdir(fold_p)
@@ -28,41 +40,31 @@ while i < len(filearray):
     if filearray[i] == 'Neg':
         f = open("negatives.txt","a+")
         f.truncate(0)
-        nums = random.sample(range(1,7897), neg_in)
+        nums = random.sample(range(1,3019), neg_in)
         while j < neg_in:
-            imNeg = 'neg_' + str(nums[j]) + '.jpg'
-            file_path = output + '/' + imNeg
-            try:#Error checking to ensure no Image is corrupt.
-                if imNeg in fold_d:
-                    im = Image.open(os.path.join(fold_p,imNeg))
-                    im.verify()
-                    im.close()
-                    shutil.copy(fold_p + '/' + imNeg, output)
-                    f.write(str(file_path).rstrip() + '\n')
-                    j = j + 1
-            except(IOError, SyntaxError) as e:
-                    print('Bad file, please remove: ',imNeg)
-                    break
+            text_p = 'neg_' + str(nums[j]) + '.jpg'
+            file_path = nfile + '/' + filearray[i] + '/' + text_p
+            if text_p in fold_d:
+                shutil.copy(fold_p + '/' + text_p, output)
+                f.write(str(file_path).rstrip() + '\n')
+                j = j + 1
+            else:
+                print(text_p + ' is not found with fold_d array.')
         f.close()
         nums = []
     if filearray[i] == 'Pos':
         f = open("positives.txt","a+")
         f.truncate(0)
-        nums = random.sample(range(1,435), pos_in)
+        nums = random.sample(range(1,pos_set), pos_in)
         while j < pos_in:
-            imPos = 'pos_' + str(nums[j]) + '.jpg'
-            file_path = output + '/' + imPos
-            try:
-                if imPos in fold_d:
-                    im = Image.open(os.path.join(fold_p,imPos))
-                    im.verify()
-                    im.close()
-                    shutil.copy(fold_p + '/' + imPos, output)
-                    f.write(str(file_path).rstrip() + '\n')
-                    j = j + 1
-            except(IOError, SyntaxError) as e:
-                print('Bad file, please remove: ',imPos)
-                break
+            text_p = 'pos_' + str(nums[j]) + file_ext
+            file_path = nfile + '/' + filearray[i] + '/' + text_p
+            if text_p in fold_d:
+                shutil.copy(fold_p + '/' + text_p, output)
+                f.write(str(file_path).rstrip() + '\n')
+                j = j + 1
+            else:
+                print('Doesnt')
         f.close()
         nums = []
     j = 0
